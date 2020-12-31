3edf63b1a4ce2a04dc3458cc4c71dc9238f882a8
Kyle McDowell <kymcd21@icstudents.org>
2020-12-31 23:03:00 +0000

    smoke test

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
+import java.util.*;
+
 class Main {
-  public static void main(String[] args) {
-    
-
-
-    
-  }
+public static void main(String[] args)
+{
+// Get the Array
+int intArr[] = { 10, 20, 15, 22, 35 };
+// To convert the elements as List
+System.out.println("Integer Array as List: "+Arrays.toString(intArr));
+}
 }
